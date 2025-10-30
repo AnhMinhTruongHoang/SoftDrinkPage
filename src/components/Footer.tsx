9c8eb2ce5bc55da67826862a88f350801cb702a3
Truong Hoang Anh Minh <154514589+AnhMinhTruongHoang@users.noreply.github.com>
2025-10-30 18:08:59 +0700

    fix ui

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ type Props = {};
 
 export default function Footer({}: Props) {
   return (
-    <footer className="bg-[#FEE832] text-[#FE6334]">
-      <div className="relative mx-auto flex w-full w-screen max-w-4xl justify-center px-0 py-0">
-        <FizziLogo />
-        <div className="absolute right-24 top-0 size-2 origin-center -translate-y-10 md:size-36 md:-translate-y-20">
+    <footer className="relative flex h-[100px] items-center justify-center bg-[#FEE832] text-[#FE6334]">
+      <div className="relative flex w-full max-w-4xl justify-center">
+        <FizziLogo className="scale-90 md:scale-100" />
+        <div className="absolute right-12 top-0 size-16 -translate-y-8 md:size-24 md:-translate-y-12">
           <CircleText />
         </div>
       </div>
